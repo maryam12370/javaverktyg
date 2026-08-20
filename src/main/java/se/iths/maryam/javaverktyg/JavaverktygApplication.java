78a688514c7a2d5ba5ef46aa9e054fa3674bc174
Ditt github-maryam12370 <Din github-maryamselemankhil786@gmail.com>
2026-08-20 17:16:31 +0200

    Initial commit

---
+++ src/main/java/se/iths/maryam/javaverktyg/JavaverktygApplication.java
@@ -0,0 +1,13 @@
+package se.iths.maryam.javaverktyg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaverktygApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaverktygApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/iths/maryam/javaverktyg/JavaverktygApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.maryam.javaverktyg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaverktygApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
